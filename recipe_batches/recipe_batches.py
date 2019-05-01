1f12f3ad2fa95ba4499c6e81666c3e6fc92140a8
Clint Kunz <clintkunz@gmail.com>
2019-05-01 16:45:34 -0600

    cookies passes small test

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -20,11 +20,11 @@ def recipe_batches(recipe, ingredients):
 
   pass 
 
-print(recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 }))
+#print(recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 }))
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  # recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  # ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  # print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
