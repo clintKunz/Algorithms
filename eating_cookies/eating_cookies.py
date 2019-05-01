ca8ad2a36f8bc50c5aed416ed2fd9832e3ea45cd
Clint Kunz <clintkunz@gmail.com>
2019-05-01 17:30:43 -0600

    passes cookie large test

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,7 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=0):
+def eating_cookies(n, cache=None):
   if n <= 0:
     return 1
   elif n == 1:
@@ -14,8 +14,13 @@ def eating_cookies(n, cache=0):
     return 2
   elif n == 3:
     return 4
+  elif cache and cache[n] > 0:
+    return cache[n]
   else: 
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if not cache:
+      cache = {i: 0 for i in range(n+1)}
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache) 
+    return cache[n]
     
 
 # n = 5
